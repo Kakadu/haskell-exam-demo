f8eed4cee86db6463998a101450d79c0d015ff17
kakadu <kakadu.hafanana@gmail.com>
2018-01-18 19:43:59 +0300

    fix tests exit code

--- tests/test-arithm.hs
+++ tests/test-arithm.hs
@@ -3,7 +3,7 @@ module Main where
 import Arithm (eval, parse)
 import Test.HUnit
 import Data.Either
-import System.Exit (exitFailure)
+import System.Exit
 
 test1 name str expected =
   TestLabel name $ TestList [c1,c2]
@@ -17,8 +17,11 @@ tests = TestList
   [ test1 "test1" "1+2*3" 7
   , test1 "test2" "5*3" 16
   ]
-main = runTestTT tests
 
--- main = do
---     putStrLn "This test always fails!"
---     exitFailure
+main = do
+  results <- runTestTT tests
+  if (errors results + failures results == 0)
+   then
+     exitWith ExitSuccess
+   else
+     exitWith (ExitFailure 1)
